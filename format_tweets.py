dc3f56a46217490a4f4af66eb72d3a405bf9a96f
Emma Keenan <38635014+ekeenan3@users.noreply.github.com>
2020-07-01 12:31:11 -0400

    add basic function to load tweets from json

--- format_tweets.py
+++ format_tweets.py
@@ -1,1 +1,18 @@
 # Turns downloaded tweets into text and hashtags for the model to use
+
+import nltk
+import json
+
+def load_tweets(path):
+    f = open(path)
+    data = json.load(f)
+    # Maybe we should make an object for this
+    tweets_text = []
+    tweets_hashtags = []
+    tweets_date = []
+    for tweet in data['tweets']:
+        tweets_text.append(tweet['extended_tweet']['full_text'])
+        tweets_hashtags.append([hashtag['text'] for hashtag in tweet['extended_tweet']['entities']['hashtags']])
+        tweets_date.append(tweet['created_at'])
+    # Yeah we might wanna make an object for this
+    return tweets_text, tweets_hashtags, tweets_date
