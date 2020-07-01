15db5f578e0254c4b0fcb1a089d446d1c3eaa3f8
Emma Keenan <38635014+ekeenan3@users.noreply.github.com>
2020-07-01 12:41:38 -0400

    change load tweets to give list of dictionaries

--- format_tweets.py
+++ format_tweets.py
@@ -6,13 +6,12 @@ import json
 def load_tweets(path):
     f = open(path)
     data = json.load(f)
-    # Maybe we should make an object for this
-    tweets_text = []
-    tweets_hashtags = []
-    tweets_date = []
+
+    tweets = []
     for tweet in data['tweets']:
-        tweets_text.append(tweet['extended_tweet']['full_text'])
-        tweets_hashtags.append([hashtag['text'] for hashtag in tweet['extended_tweet']['entities']['hashtags']])
-        tweets_date.append(tweet['created_at'])
-    # Yeah we might wanna make an object for this
-    return tweets_text, tweets_hashtags, tweets_date
+        tweets.append({})
+        tweets['text'] = tweet['extended_tweet']['full_text']
+        tweets['hashtags'] = [hashtag['text'] for hashtag in tweet['extended_tweet']['entities']['hashtags']]
+        tweets['id'] = int(tweet['id_str']))
+
+    return tweets
