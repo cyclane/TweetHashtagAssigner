badd14b8614b28334dbf8a0e1425281050d966eb
Emma Keenan <38635014+ekeenan3@users.noreply.github.com>
2020-07-01 12:44:31 -0400

    add comments

--- format_tweets.py
+++ format_tweets.py
@@ -4,9 +4,12 @@ import nltk
 import json
 
 def load_tweets(path):
+    # Loads json file of tweets from the given path
     f = open(path)
     data = json.load(f)
 
+    # Creates a list of tweets where each tweet is a dictionary of its text
+    # hashtags and id
     tweets = []
     for tweet in data['tweets']:
         tweets.append({})
