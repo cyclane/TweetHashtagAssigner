809ccb548ff5782cac4be8aec15d4431332f59f6
Emma Keenan <38635014+ekeenan3@users.noreply.github.com>
2020-07-02 13:48:19 -0400

    Add tokenizing and lemmatizing and remove load tweets

--- format_tweets.py
+++ format_tweets.py
@@ -1,27 +1,28 @@
-# Turns downloaded tweets into text and hashtags for the model to use
+# Turns text of a tweet into the important words that are tokenized and lemmatized
 
 import nltk
-import json
-
-def load_tweets(path):
-    # Loads json file of tweets from the given path
-    f = open(path)
-    data = json.load(f)
-
-    # Creates a list of tweets where each tweet is a dictionary of its text
-    # hashtags id and lemmatized important words
-    tweets = []
-    for index, tweet in enumerate(data['tweets']):
-        tweets.append({})
-        tweets[index]['text'] = tweet['extended_tweet']['full_text']
-        tweets[index]['hashtags'] = [hashtag['text'] for hashtag in tweet['extended_tweet']['entities']['hashtags']]
-        tweets[index]['id'] = int(tweet['id_str'])
-        tweets[index]['words'] = process_text(tweets[index]['text'])
-    return tweets
+from nltk.tokenize import TweetTokenizer
+from nltk.stem import wordnet as wn
 
 def process_text(text):
-    # Tokenize the text into words with nltk
-    # Remove unimportant words like 'the'
+    # Tokenize tweet into words
+    # NOTE: I'm not sure TweetTokenizer is actually useful here, might just change it to word tokenizer later
+    tokenizer = TweetTokenizer(preserve_case=False)
+    tokens = tokenizer.tokenize(text)
+
     # Lemmatize the words
+    lemmatizer = wn.WordNetLemmatizer()
+    simple_words = [lemmatizer.lemmatize(token) for token in tokens]
+
+    # Tag simplified words with part of speech
+    word_tags = nltk.pos_tag(simple_words)
+
+    # Remove conjunctions, determiners, etc.
+    # Full list of what the tags mean at https://medium.com/@muddaprince456/categorizing-and-pos-tagging-with-nltk-python-28f2bc9312c3
+    important_words = []
+    for i, word_tag in enumerate(word_tags):
+        if word_tag[1] not in {'CC', 'DT', 'EX', 'IN', 'LS', 'MD', 'PDT', 'POS', 'PRP', 'PRP$', 'RP', 'TO', 'UH', 'WDT', 'WP', 'WP$', 'WRB'}:
+            important_words.append(simple_words[i])
+
     # Return array of lemmatized important words
-    pass
+    return important_words
