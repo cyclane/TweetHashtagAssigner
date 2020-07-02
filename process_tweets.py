5d464cb962148c575432beb455462fbccba2e07b
Emma Keenan <38635014+ekeenan3@users.noreply.github.com>
2020-07-02 14:15:26 -0400

    add process text to process tweets

--- process_tweets.py
+++ process_tweets.py
@@ -2,11 +2,37 @@
 # Bayes theorem with some laplace smoothing and word similarity and lemmatization
 
 import nltk
+from nltk.tokenize import TweetTokenizer
+from nltk.stem import wordnet as wn
 
 # Load data somewhere and get unique word count
 unique_word_count = 1
 # List of all possible hashtags
 list_of_hashtags = []
+
+# Turns text into a list of lemmatized, important words
+def process_text(text):
+    # Tokenize tweet into words
+    # NOTE: I'm not sure TweetTokenizer is actually useful here, might just change it to word tokenizer later
+    tokenizer = TweetTokenizer(preserve_case=False)
+    tokens = tokenizer.tokenize(text)
+
+    # Lemmatize the words
+    lemmatizer = wn.WordNetLemmatizer()
+    simple_words = [lemmatizer.lemmatize(token) for token in tokens]
+
+    # Tag simplified words with part of speech
+    word_tags = nltk.pos_tag(simple_words)
+
+    # Remove conjunctions, determiners, etc.
+    # Full list of what the tags mean at https://medium.com/@muddaprince456/categorizing-and-pos-tagging-with-nltk-python-28f2bc9312c3
+    important_words = []
+    for i, word_tag in enumerate(word_tags):
+        if word_tag[1] not in {'CC', 'DT', 'EX', 'IN', 'LS', 'MD', 'PDT', 'POS', 'PRP', 'PRP$', 'RP', 'TO', 'UH', 'WDT', 'WP', 'WP$', 'WRB'}:
+            important_words.append(simple_words[i])
+
+    # Return array of lemmatized important words
+    return important_words
 
 # Returns list of hashtags with value for how probable they are for given tweet
 def hashtag_assigner(tweet):
@@ -20,7 +46,7 @@ def hashtag_assigner(tweet):
 def bayes_model(hashtag, tweet):
     # Find probability of the text given the hashtag
     probability_of_text = 1
-    for word in tweet['text']:
+    for word in process_text(tweet['text']):
         probability_of_text *= word_probability(word, hashtag)
 
     # Finds the probability of the hashtag given the timestamp
