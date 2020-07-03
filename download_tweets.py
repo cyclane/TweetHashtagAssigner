1c168ea805553e0b1d4a7ff6b129faaa2fb12f74
Gleb Koval <glebkoval4@gmail.com>
2020-07-03 06:39:35 +0100

    Changed download to save to database

--- download_tweets.py
+++ download_tweets.py
@@ -1,23 +1,21 @@
 # Downloads tweets and puts them in format to go to process_tweets.py
 
-import sys, argparse, json, random, urllib.parse, time
+import sys, argparse, json, random, urllib.parse, time, copy, threading
 try:
     import requests
     from requests.auth import AuthBase
 except:
     print("This script requires the \"requests\" package!")
-
 try:
     import nltk
 except:
     print("This script requires the \"nltk\" package!")
+try:
+    import mysql.connector
+except:
+    print("This script requires the \"mysql-connector-python\" package!")
 
-def drawProgressBar(percent, bar_length=20):
-    sys.stdout.write("\r")
-    sys.stdout.write("[{:<{}}] {:.0f}%".format("=" * int(bar_length * percent), bar_length, percent * 100))
-    sys.stdout.flush()
-
-# (Copied from Twitter)
+# (Copied from Twitter docs)
 # Generates a bearer token with consumer key and secret via https://api.twitter.com/oauth2/token.
 class BearerTokenAuth(AuthBase):
     def __init__(self, consumer_key, consumer_secret):
@@ -44,8 +42,8 @@ class BearerTokenAuth(AuthBase):
         r.headers['User-Agent'] = 'LabsResearchSearchQuickStartPython'
         return r
 
-def search_request(bearer_token, query):
-    url = f"https://api.twitter.com/1.1/search/tweets.json?q={urllib.parse.quote(query)}&count=100"
+def tweets_search_request(bearer_token, query):
+    url = f"https://api.twitter.com/1.1/search/tweets.json?q={urllib.parse.quote(query)}&count=100&tweet_mode=extended"
     headers = {"Accept-Encoding": "gzip"}
     response = requests.get(url, auth=bearer_token, headers=headers)
     if response.status_code != 200:
@@ -62,12 +60,23 @@ def remove_repeated_tweets(tweets):
         if not tweet["id_str"] in exists:
             exists.append(tweet["id_str"])
             non_repeated_tweets.append(tweet)
-
     return non_repeated_tweets
 
+def save(database, tweets):
+    cursor = database.cursor()
+    for tweet in tweets:
+        cursor.execute(
+            "INSERT INTO tweets (id, content, hashtags) VALUES (%s, %s, %s)",
+            (tweet["id_str"], tweet["full_text"], ",".join([ hashtag["text"] for hashtag in tweet["entities"] ]))
+        )
+    database.commit()
+    print(f"{len(tweets)} tweets saved to database")
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--output","-o",help="Name of output file (JSON)",default="tweets.json")
+parser.add_argument("--host","-h",help="Hostname of output database",defautl="127.0.0.1")
+parser.add_argument("--database","-d",help="Name of database to use",default="tweets")
+parser.add_argument("--user","-u",help="Database user to login with",required=True)
+parser.add_argument("--password","-p",help="Database password for user",required=True)
 parser.add_argument("--key","-k",help="Consumer API Key",required=True)
 parser.add_argument("--secret","-s",help="Consumer API Secret",required=True)
 args = parser.parse_args()
@@ -80,24 +89,23 @@ if __name__ == "__main__":
         print("Twitter successfully authorised!")
     except Exception as error:
         print(error)
-
     print("Press ctrl + c to end the script, otherwise it well keep downloading at an interval of 1 request every 2 seconds (rate limit)")
 
-    tweets = []
+    database = mysql.connector.connect(
+        host=args.host,
+        user=args.user,
+        password=args.password,
+        database=args.database
+    )
+    database_cursor = database.cursor()
 
     try:
         while True:
-            tweets += search_request(bearer_token, get_random_query())
+            tweets = tweets_search_request(bearer_token, get_random_query())
+            save_thread = threading.Thread(target=save,args=(database_cursor, tweets))
             time.sleep(2) # Stay within the rate limit of 1 request every 2 seconds
     except KeyboardInterrupt:
-        print(f"Script interupted! Saving tweets to \"{args.output}\"")
+        print(f"Script interupted!")
     except Exception as e:
-        print(f"Script errored! Saving tweets to \"{args.output}\"\n\n{e}")
-
-    # Filter repeated tweets
-    tweets = remove_repeated_tweets(tweets)
-
-    # Save tweets
-    with open(args.output, "w") as output:
-        json.dump(tweets, output)
+        print(f"Script errored!\n\n{e}")
 
---
+++ tweets.sql
@@ -0,0 +1,5 @@
+CREATE TABLE tweets (
+    id BIGINT UNSIGNED UNIQUE,
+    content VARCHAR(280),
+    hashtags VARCHAR(280)
+);
