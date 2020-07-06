b78f3695d4d71842c1dd61fa350882d7f9c70c82
Gleb Koval <glebkoval4@gmail.com>
2020-07-06 21:26:43 +0100

    Fixed

--- website/assigner.js
+++ website/assigner.js
@@ -1,7 +1,9 @@
 function getHashtags(){
     let text = document.getElementById("tweet").value
     fetch(`/api/probability?text=${encodeURIComponent(text)}`).then(function (response) {
-        let hashtags = response.json()["hashtags"]
+        return response.json()
+    }).then(function (data) {
+        let hashtags = data["hashtags"]
         let children = document.getElementById("hashtags").children
 
         for (let index=0; index < children.length; index++) {
